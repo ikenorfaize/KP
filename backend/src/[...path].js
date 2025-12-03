abb51520b6594562c048659f4009a78e80a08718
fairu <fairuz.fuadi04@gmail.com>
2025-12-03 14:57:54 +0700

    bersih file

--- backend/src/[...path].js
+++
@@ -1,6 +0,0 @@
-// Catch-all serverless function for Vercel
-// This wraps the Express app from index.js to handle all /api/* routes properly
-import app from './index.js';
-
-// Export the Express app as serverless function handler
-export default app;
